63e0b794d95e700f38b3f55f3a461eddca55e7c9
Luiz Oliveira <luiz.oliveira@foton.la>
2022-11-22 04:52:31 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+function AnimesListaTela(){
+  return(
+    <View>
+      <Text>Meus Animesa</Text>
+      </View>
+  )
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Apps</Text>
+      <AnimesListaTela></AnimesListaTela>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +23,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  tituloTela: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  containerAnimesLista: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "black",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  tituloAnimesLista: {
+    fontSize: 20,
+  }
 });
